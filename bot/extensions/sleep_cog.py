9c6ee5be326cbe207fbcaf9d8d6f9f3bafd9a2a8
nathan <nathan@DESKTOP-DKF4V36>
2021-06-05 12:24:48 -0700

    test sleep cog

---
+++ bot/extensions/sleep_cog.py
@@ -0,0 +1,18 @@
+import typing
+import discord
+from discord.ext import commands
+import time
+
+def setup(bot):
+    bot.add_cog(Sleep(bot))
+
+class Sleep(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    def sleep(self, ctx, *args):
+        async with ctx.channel.typing():
+            time.sleep(5)
+            return
