9c277f218fd13787e4bd05521a424b1d92dcf996
nathan <nathan@DESKTOP-DKF4V36>
2021-07-01 21:53:05 -0700

    made youtube link checking cleaner in the code

--- bot/extensions/shitpost_cog.py
+++ bot/extensions/shitpost_cog.py
@@ -19,14 +19,35 @@ class Shitpost(commands.Cog):
     async def on_message(self, message):
         words = message.content.split(" ")
 
+        mobile_str = "https://m.youtube.com/watch?v="
         watch_str = "https://www.youtube.com/watch?v="
         embed_str = "https://www.youtube.com/embed/"
         short_str = "https://youtu.be/"
         y2u_str = "http://y2u.be/"
 
+        link_prefixes = [
+            "http://m.youtube.com/watch?v=",
+            "https://m.youtube.com/watch?v=",
+            "http://www.youtube.com/watch?v=",
+            "https://www.youtube.com/watch?v=",
+            "http://www.youtube.com/embed/",
+            "https://www.youtube.com/embed/",
+            "http://youtu.be/",
+            "https://youtu.be/",
+            "http://y2u.be/",
+            "https://y2u.be/",
+            "http://www.youtube.com/v/",
+            "https://www.youtube.com/v/"
+        ]
+
         url = None
         for w in words:
-            if w.find(watch_str) != -1:
+            for l in link_prefixes:
+                if w.find(l) != -1:
+                    index = w.index(l)
+                    url = w[index + len(l): index + len(l) + 11]
+
+            """if w.find(watch_str) != -1:
                 index = w.index(watch_str)
                 url = w[index:index + len(watch_str) + 11] # this is in case words letters are before or after the link
             
@@ -42,8 +63,12 @@ class Shitpost(commands.Cog):
                 index = w.index(y2u_str)
                 url = w[index:index + len(y2u_str) + 11]
 
+            elif w.find(mobile_str) != -1:
+                index = w.index(mobile_str)
+                url = w[index:index + len(mobile_str) + 11]"""
+
         if url is not None:
-            video_id = url.replace(watch_str, "").replace(embed_str, "")
+            video_id = url
 
             video = api.get_video_by_id(video_id=video_id)
 
